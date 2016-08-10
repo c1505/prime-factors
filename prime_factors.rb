a35698e781a022f3a6f8b7f2daf77f98209b5153
c1505 <coreym247@gmail.com>
2016-08-10 17:36:31 -0400

    all tests passing

--- prime_factors.rb
+++ prime_factors.rb
@@ -4,22 +4,23 @@ class PrimeFactors
   def self.for(arg)
     @@array_of_factors = []
     return [] if arg < 2
-    numerator = arg
+    @@numerator = arg
+    count = 0
     
-    numerator = self.one_time(numerator)
-    unless numerator == 1
-      numerator = self.one_time(numerator)
+    until @@numerator == 1 || count == 20
+      @@numerator = self.one_time
+      count += 1
     end
     @@array_of_factors
-    
+
   end
   
-  def self.one_time(numerator)
+  def self.one_time
     divisor = 2
-    until numerator % divisor == 0
+    until @@numerator % divisor == 0
       divisor += 1
     end
     @@array_of_factors << divisor
-    numerator / divisor
+    @@numerator = @@numerator / divisor
   end
 end
